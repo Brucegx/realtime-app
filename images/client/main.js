a2d4d7ba35c40100ce9a8cfee60a0f16d56ee948
Brucegx <guoxi.bruce@gmail.com>
2017-09-29 17:01:21 -0400

    add react-dom

--- images/client/main.js
+++ images/client/main.js
@@ -1,22 +1,11 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+// any Js in here is automatically ran for us
 
-import './main.html';
+import React from 'react';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
-
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
+const App = () => {
+    return (
+        <div>
+            React App #2
+        </div>
+    );
+};
