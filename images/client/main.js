38e95272a7dec64cc1e254f1b8dc163afb762155
Brucegx <guoxi.bruce@gmail.com>
2017-09-30 02:00:45 -0400

    import axios to implementing AJAX

--- images/client/main.js
+++ images/client/main.js
@@ -2,7 +2,9 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 import ImageList from './components/image_list';
+
 const App = () => {
     return (
         <div>
@@ -13,4 +15,6 @@ const App = () => {
 
 Meteor.startup(() => {
     ReactDOM.render(<App />, document.querySelector('.container'));
+    axios.get("https://api.imgur.com/3/gallery/hot/viral/0")
+        .then(response => console.log(response));
 });
