efd71ed02333159bc12e35464e173ea23813f9d2
Brucegx <guoxi.bruce@gmail.com>
2017-09-29 20:12:06 -0400

    demo tw:bootstrap

--- images/client/components/image_list.js
+++ images/client/components/image_list.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import ImageDetail from './image_detail';
+
+const IMAGES = [
+    {title: 'Pen', link: 'https://dummyimage.com/600x400'},
+    {title: 'Pine Tree', link: 'https://dummyimage.com/600x400'},
+    {title: 'Mug', link: 'https://dummyimage.com/600x400'}
+];
 
 const ImageList = () => {
+    const RenderedImages = IMAGES.map(image =>
+        <ImageDetail key={image.title} image={image} />
+    );
+    
     return (
-        <ul>
-            <li>Image Detail</li>
+        <ul className="media-list list-group">
+            {RenderedImages}
         </ul>
     );
 };
