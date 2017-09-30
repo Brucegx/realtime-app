93dc9425443ee6300580925d1144fc43517a6281
Brucegx <guoxi.bruce@gmail.com>
2017-09-30 12:07:53 -0400

    parsing images through props from parent to child component

--- images/client/main.js
+++ images/client/main.js
@@ -6,15 +6,19 @@ import axios from 'axios';
 import ImageList from './components/image_list';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {images: []};
+    }
     componentWillMount() {
         axios.get("https://api.imgur.com/3/gallery/hot/viral/0")
-        .then(response => console.log(response)); 
+            .then(response => this.setState({images: response.data.data})); 
     }
 
     render() {
         return (
             <div>
-                <ImageList /> 
+                <ImageList image={this.state.images}/> 
             </div>
         );
     };
