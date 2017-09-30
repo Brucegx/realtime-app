6618d4e28ce76d31380e9495249b7b4c512c2ba3
Brucegx <guoxi.bruce@gmail.com>
2017-09-30 02:22:48 -0400

    using clasee instead of const and componentWillMount

--- images/client/main.js
+++ images/client/main.js
@@ -1,20 +1,25 @@
 // any Js in here is automatically ran for us
 
-import React from 'react';
+import React, { Component }from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 import ImageList from './components/image_list';
 
-const App = () => {
-    return (
-        <div>
-            <ImageList /> 
-        </div>
-    );
+class App extends Component {
+    componentWillMount() {
+        axios.get("https://api.imgur.com/3/gallery/hot/viral/0")
+        .then(response => console.log(response)); 
+    }
+
+    render() {
+        return (
+            <div>
+                <ImageList /> 
+            </div>
+        );
+    };
 };
 
 Meteor.startup(() => {
     ReactDOM.render(<App />, document.querySelector('.container'));
-    axios.get("https://api.imgur.com/3/gallery/hot/viral/0")
-        .then(response => console.log(response));
 });
