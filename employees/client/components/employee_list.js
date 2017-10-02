0e59e6745b3d19524a6c5109e11611f81952e387
Brucegx <guoxi.bruce@gmail.com>
2017-10-02 02:03:10 -0400

    add button with class based component

--- employees/client/components/employee_list.js
+++ employees/client/components/employee_list.js
@@ -1,22 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
 import { Employees } from '../../imports/collections/employees';
 import EmployeeDetail from './employee_detail';
 
-const EmployeeList = (props) => {
-    console.log(props.employees);
-    return (
+const PER_PAGE = 20;
+
+class EmployeeList extends Component {
+    render() {
+        return (
         <div>
             <div className="employee-list">
-                {props.employees.map(employee => <EmployeeDetail employee={employee}/>)}
+                {this.props.employees.map(employee => 
+                    <EmployeeDetail key={employee._id} employee={employee}/>)}
             </div>
+            <button onClick={() => Meteor.subscribe('employees', 40)} className="btn btn-primary">
+                Load More...
+            </button>
         </div>
-    );
+        );
+    };
 };
 
 export default createContainer(() => {
     // set up subscription
-    Meteor.subscribe('employees');
+    Meteor.subscribe('employees', PER_PAGE);
     //return an object. it will be sent to EmployeeList as props
     return { employees: Employees.find({}).fetch() }; 
 }, EmployeeList);
