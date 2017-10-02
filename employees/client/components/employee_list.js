6c9de39edac8ef41762c48b5764aba45964a2fda
Brucegx <guoxi.bruce@gmail.com>
2017-10-02 11:51:18 -0400

    meteor demon done

--- employees/client/components/employee_list.js
+++ employees/client/components/employee_list.js
@@ -6,14 +6,21 @@ import EmployeeDetail from './employee_detail';
 const PER_PAGE = 20;
 
 class EmployeeList extends Component {
+    componentWillMount() {
+        this.page = 1;
+    }
+    handleButtonClick() {
+        Meteor.subscribe('employees', PER_PAGE * this.page);
+        this.page += 1;
+    }
     render() {
         return (
         <div>
-            <div className="employee-list">
+            <div className="employee-list">   
                 {this.props.employees.map(employee => 
                     <EmployeeDetail key={employee._id} employee={employee}/>)}
             </div>
-            <button onClick={() => Meteor.subscribe('employees', 40)} className="btn btn-primary">
+            <button onClick={this.handleButtonClick.bind(this)} className="btn btn-primary">
                 Load More...
             </button>
         </div>
